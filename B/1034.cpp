530f35fc420774aaeadddcf8e5888a958a0051e3
Jiajian Chen <chenjiajia9411@users.noreply.github.com>
2017-03-12 00:18:54 +0800

    Update 1034.cpp

--- B/1034.cpp
+++ B/1034.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-string itos(int i)
+string itos(long long i)
 {
     stringstream ss;
     ss << i;
@@ -14,9 +14,9 @@ string itos(int i)
     return s;
 }
 
-int stoi(string s)
+long long ston(string s)
 {
-    int i;
+    long long i;
     stringstream ss(s);
     ss >> i;
     ss.clear();
@@ -24,81 +24,87 @@ int stoi(string s)
     return i;
 }
 
-int gcd(int a,int b)
+long long gcd(long long a,long long b)
 {
     if(a == b) return b;
     else if(a < b) return gcd(a,b-a);
     else return gcd(a-b,b);
 }
 
-int lcm(int a,int b)
+long long lcm(long long a,long long b)
 {
     return (a*b)/gcd(a,b);
 }
 
-string simplification(int a1,int b1)
+string simplification(long long a,long long b)
 {
-    if(a1*b1 == 0) return "0";
-    else if(abs(a1)/abs(b1) != 0)
+    if(!(a*b)) return "0";
+    else if(abs(a) / abs(b))
     {
-        if(abs(a1)%abs(b1) == 0 && a1*b1 > 0)
-            return itos(abs(a1)/abs(b1));
-        else if(abs(a1)%abs(b1) == 0 && a1*b1 < 0)
-            return "(-"+itos(abs(a1)/abs(b1))+")";
-        else if(abs(a1)%abs(b1) != 0 && a1*b1 > 0)
-            return itos(abs(a1)/abs(b1))+" "+itos(a1 - a1%b1)+"/"+itos(b1);
-        else if(abs(a1)/abs(b1) != 0 && a1*b1 < 0)
-            return "(-"+itos(abs(a1)/abs(b1))+" "+itos(a1 - a1%b1)+"/"+itos(b1)+")";
+        if(abs(a) % abs(b))
+        {
+            if(a*b > 0) return itos(abs(a)/abs(b))+" "+simplification(abs(a)%abs(b),abs(b));
+            else return "(-"+itos(abs(a)/abs(b))+" "+simplification(abs(a)%abs(b),abs(b))+")";
+        }
+        else
+        {
+            if(a*b > 0) return itos(abs(a)/abs(b));
+            else return "(-"+itos(abs(a)/abs(b))+")";
+        }
     }
-    else if(abs(a1)/abs(b1) == 0 && a1*b1 < 0)
-        return "(-"+itos(abs(a1))+"/"+itos(abs(b1))+")";
-    else return itos(abs(a1))+"/"+itos(abs(b1));
+    else
+    {
+        long long abgcd = gcd(abs(a),abs(b));
+        a /= abgcd;b /= abgcd;
+        if(a*b > 0) return itos(abs(a))+"/"+itos(abs(b));
+        else return "(-"+itos(abs(a))+"/"+itos(abs(b))+")";
+    }
 }
 
-int numerator(string a)
+long long numerator(string a)
 {
-    return stoi(a);
+    return ston(a);
 }
 
-int denominator(string a)
+long long denominator(string a)
 {
-    return stoi(a.substr(a.find("/") + 1));
+    return ston(a.substr(a.find("/") + 1));
 }
 
-string addition(int a1,int a2,int b1,int b2)
+string addition(long long a1,long long a2,long long b1,long long b2)
 {
     if(!a1) return simplification(b1,b2);
     else if(!b1) return simplification(a1,a2);
     else
     {
-        int common_denominator = lcm(a2,b2);
+        long long common_denominator = lcm(a2,b2);
         a1 *= common_denominator/a2;b1 *= common_denominator/b2;
-        int result_numerator = (a1 + b1) / gcd(a1 + b1,common_denominator);
-        int result_denominator = common_denominator / gcd(a1 + b1,common_denominator);
+        long long result_numerator = (a1 + b1) / gcd(abs(a1 + b1),abs(common_denominator));
+        long long result_denominator = common_denominator / gcd(abs(a1 + b1),abs(common_denominator));
         return simplification(result_numerator,result_denominator);
     }
 }
 
-string subtraction(int a1,int a2,int b1,int b2)
+string subtraction(long long a1,long long a2,long long b1,long long b2)
 {
     if(!a1) return simplification(-b1,-b2);
     else if(!b1) return simplification(a1,a2);
     else
     {
-        int common_denominator = lcm(a2,b2);
+        long long common_denominator = lcm(a2,b2);
         a1 *= common_denominator/a2;b1 *= common_denominator/b2;
-        int result_numerator = (a1 - b1) / gcd(a1 - b1,common_denominator);
-        int result_denominator = common_denominator / gcd(a1 - b1,common_denominator);
+        long long result_numerator = (a1 - b1) / gcd(abs(a1 - b1),abs(common_denominator));
+        long long result_denominator = common_denominator / gcd(abs(a1 - b1),abs(common_denominator));
         return simplification(result_numerator,result_denominator);
     }
 }
 
-string multiplication(int a1,int a2,int b1,int b2)
+string multiplication(long long a1,long long a2,long long b1,long long b2)
 {
     return simplification(a1*b1,a2*b2);
 }
 
-string division(int a1,int a2,int b1,int b2)
+string division(long long a1,long long a2,long long b1,long long b2)
 {
     if(!b1) return "Inf";
     else return simplification(a1*b2,a2*b1);
@@ -106,10 +112,10 @@ string division(int a1,int a2,int b1,int b2)
 
 int main()
 {
-	string a,b;
-	cin>>a>>b;
-    int a1 = numerator(a);int a2 = denominator(a);
-    int b1 = numerator(b);int b2 = denominator(b);
+    string a,b;
+    cin>>a>>b;
+    long long a1 = numerator(a);long long a2 = denominator(a);
+    long long b1 = numerator(b);long long b2 = denominator(b);
     string as = simplification(a1,a2);string bs = simplification(b1,b2);
     cout<<as<<" + "<<bs<<" = "<<addition(a1,a2,b1,b2)<<endl;
     cout<<as<<" - "<<bs<<" = "<<subtraction(a1,a2,b1,b2)<<endl;
