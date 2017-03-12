9ab349071a0a49f49aef7fd51330dfad12973785
Jiajian Chen <chenjiajia9411@users.noreply.github.com>
2017-03-12 19:01:17 +0800

    Update 1024.cpp

--- B/1024.cpp
+++ B/1024.cpp
@@ -1,48 +1,57 @@
+//指数可能很大，double搞不掂，只能上deque.......
+
+
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <cmath>
+#include <algorithm>
+#include <deque>
 using namespace std;
-
-void printDouble(bool flag,double base,int index,int inputprecision,int realprecision)
-{
-    cout.setf(ios::fixed);cout.fill('0');
-    if(flag)
-    {
-        if(realprecision - index > 0 && base - (int)base != 0) cout<<setprecision(realprecision-index)<<base*pow(10,index)<<endl;
-        else cout<<setprecision(0)<<base*pow(10,index)<<endl;
-    }
-    else cout<<setprecision(inputprecision+index)<<base*pow(10,-index)<<endl;
-}
-
-int getRealprecision(double base)
-{
-    stringstream ss;
-    ss << base;
-    string s;
-    ss >> s;
-    ss.clear();
-    ss.str("");
-    return s.length() - s.find(".") - 1;
-}
 
 int main()
 {
-	string a;
-	cin>>a;
-	bool flag1 = true;
-	if(a[0] == '-') flag1 = false;
-    stringstream ss;
-    ss << a.substr(1,a.find('E')-1);
-    double base;
-    ss >> base;
-    bool flag2 = true;
-    if(a[a.find('E')+1] == '-') flag2 = false;
-    ss.clear();
-    ss.str("");
-    ss << a.substr(a.find('E')+2,a.length()-a.find('E')-2);
+    char sign;
+    cin>>sign;
+    if(sign == '-') cout<<"-";
+    deque<char> base;char buffer;
+    for(; (buffer = cin.get()) && (buffer != 'E');) base.push_back(buffer);
     int index;
-    ss >> index;
-    if(flag1) printDouble(flag2,base,index,a.find('E')-a.find('.')-1,getRealprecision(base));
-    else {cout<<"-";printDouble(flag2,base,index,a.find('E')-a.find('.')-1,getRealprecision(base));}
+    cin>>sign>>index;
+    if(index)
+    {
+        if(sign == '+')
+        {
+            if(index > base.end() - find(base.begin(),base.end(),'.') - 1)
+            {
+                base.erase(base.begin()+1);
+                int difference = index - base.size() + 1;
+                for(int i = 0; i < difference; i++) base.push_back('0');
+            }
+            else
+            {
+                base.erase(base.begin()+1);
+                int difference = base.size() - 1- index;
+                for(int i = 0; i < difference; i++)
+                {
+                    buffer = *(base.end()-1);
+                    base.pop_back();
+                    base.push_front(buffer);
+                }
+                if(difference) base.push_back('.');
+                for(int i = 0; i < difference; i++)
+                {
+                    buffer = *base.begin();
+                    base.pop_front();
+                    base.push_back(buffer);
+                }
+            }
+        }
+        else if(sign == '-')
+        {
+            base.erase(base.begin()+1);
+            for(int i = 0; i < index - 1; i++) base.push_front('0');
+            base.push_front('.');
+            base.push_front('0');
+        }
+    }
+    for(deque<char>::iterator i = base.begin(); i != base.end(); i++) cout<<*i;
+    cout<<endl;
 }
