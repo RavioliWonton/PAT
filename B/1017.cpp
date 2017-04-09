44b4bc4371a03321e8c49e3b90943c20899ee679
Jiajian Chen <chenjiajia9411@users.noreply.github.com>
2017-04-09 15:06:13 +0800

    Clean up.

--- B/1017.cpp
+++ B/1017.cpp
@@ -1,43 +1,31 @@
 #include <iostream>
-#include <sstream>
 #include <string>
 using namespace std;
 
-string itos(int i)
-{
-    stringstream ss;
-    ss << i;
-    string s;
-    ss >> s;
-    ss.clear();
-    ss.str("");
-    return s;
-}
-
 int main()
 {
-    string dividend;int divisor;
-    cin>>dividend>>divisor;
-    string quotient = "";int remainder = 0;
-    if(dividend.size() == 1)
+	string dividend;int divisor;
+	cin>>dividend>>divisor;
+	string quotient = "";int remainder = 0;
+	if(dividend.size() == 1)
     {
-        quotient = itos(stoi(dividend) / divisor);
+        quotient = to_string(stoi(dividend) / divisor);
         remainder = stoi(dividend) % divisor;
     }
-    else
-    {
+	else
+	{
         for(int i = 0;i < dividend.size();i++)
         {
             remainder *= 10;
             remainder += stoi(dividend.substr(i,1));
             if(remainder / divisor)
             {
-                quotient.append(itos(remainder / divisor));
+                quotient.append(to_string(remainder / divisor));
                 remainder %= divisor;
             }
             else quotient.append("0");
         }
-        if(quotient.substr(0,1) == "0") quotient.erase(0,1);
+        if(quotient.front() == '0') quotient.erase(0,1);
     }
     cout<<quotient<<" "<<remainder<<endl;
 }
