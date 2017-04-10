5d56cf4ca174a47305d5b2be810ee354e7d80c62
Jiajian Chen <chenjiajia9411@users.noreply.github.com>
2017-04-10 19:02:16 +0800

    Clean up.

--- B/1007.cpp
+++ B/1007.cpp
@@ -10,13 +10,21 @@ int main()
     bool (*a) = new bool[n+1];
     memset(a,true,sizeof(bool)*(n+1));
     for (int i = 2; i <= sqrt(n); i++)
-        if (a[i]) for (int j = i; j*i <= n; j++) a[j*i] = false;
-    int (*p) = new int[n];int countp = 0;
-    for (int k = 2; k <= n; k++)
-        if (a[k]) {p[countp] = k;countp++;}
-    int countn = 0;
-    for(int l = 0; l < countp; l++)
-        if(p[l+1] - p[l] == 2) countn++;
+        if (a[i]) for(int j = i; j*i <= n; j++) a[j*i] = false;
+    int first = 0;int last = 0;int countn = 0;
+    for(int i = 2; i < n + 1; i++)
+    {
+        if(a[i])
+        {
+            if(!first) first = i;
+            else
+            {
+                if(last) first = last;
+                last = i;
+                if(last - first == 2) countn++;
+            }
+        }
+    }
     cout<<countn<<endl;
-    delete [] a;delete [] p;
+    delete [] a;
 }
