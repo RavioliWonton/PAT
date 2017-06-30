0d684d67241c890693258f1430a6d9b955b6320f
Jiajian Chen <chenjiajia9411@vip.qq.com>
2017-06-30 22:53:50 +0800

    clean

--- B/1034.cpp
+++ B/1034.cpp
@@ -1,30 +1,8 @@
 //题目保证正确的输出中没有超过整型范围的整数，可没说计算过程中没有.........
 
 #include <iostream>
-#include <sstream>
-#include <cstdlib>
+#include <string>
 using namespace std;
-
-string itos(long long i)
-{
-    stringstream ss;
-    ss << i;
-    string s;
-    ss >> s;
-    ss.clear();
-    ss.str("");
-    return s;
-}
-
-long long ston(string s)
-{
-    long long i;
-    stringstream ss(s);
-    ss >> i;
-    ss.clear();
-    ss.str("");
-    return i;
-}
 
 long long gcd(long long a,long long b)
 {
@@ -33,17 +11,17 @@ long long gcd(long long a,long long b)
 
 string simplification(long long a,long long b)
 {
-    if(!(a*b)) return "0";
+    if(!(a&&b)) return "0";
     else if(abs(a) / abs(b))
     {
-        if(abs(a) % abs(b)) return (a*b > 0 ? itos(abs(a)/abs(b))+" "+simplification(abs(a)%abs(b),abs(b)) : "(-"+itos(abs(a)/abs(b))+" "+simplification(abs(a)%abs(b),abs(b))+")");
-        else return (a*b > 0 ? itos(abs(a)/abs(b)) : "(-"+itos(abs(a)/abs(b))+")");
+        if(abs(a) % abs(b)) return (a*b > 0 ? to_string(abs(a)/abs(b))+" "+simplification(abs(a)%abs(b),abs(b)) : "(-"+to_string(abs(a)/abs(b))+" "+simplification(abs(a)%abs(b),abs(b))+")");
+        else return (a*b > 0 ? to_string(abs(a)/abs(b)) : "(-"+to_string(abs(a)/abs(b))+")");
     }
     else
     {
         long long abgcd = gcd(abs(a),abs(b));
         a /= abgcd;b /= abgcd;
-        return (a*b > 0 ? itos(abs(a))+"/"+itos(abs(b)) : "(-"+itos(abs(a))+"/"+itos(abs(b))+")");
+        return (a*b > 0 ? to_string(abs(a))+"/"+to_string(abs(b)) : "(-"+to_string(abs(a))+"/"+to_string(abs(b))+")");
     }
 }
 
@@ -51,8 +29,8 @@ int main()
 {
 	string a,b;
 	cin>>a>>b;
-    long long a1 = ston(a);long long a2 = ston(a.substr(a.find("/") + 1));
-    long long b1 = ston(b);long long b2 = ston(b.substr(b.find("/") + 1));
+    long long a1 = stoll(a);long long a2 = stoll(a.substr(a.find("/") + 1));
+    long long b1 = stoll(b);long long b2 = stoll(b.substr(b.find("/") + 1));
     string as = simplification(a1,a2);string bs = simplification(b1,b2);
     cout<<as<<" + "<<bs<<" = "<<simplification(a1*b2+a2*b1,a2*b2)<<endl;
     cout<<as<<" - "<<bs<<" = "<<simplification(a1*b2-a2*b1,a2*b2)<<endl;
--- B/1052.cpp
+++ B/1052.cpp
@@ -1,53 +1,46 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-string* createList(string a)
+vector<string> createList(string& a)
 {
-    string (*p) = new string[11];int countn = 1;
-    for(int i = 0;i < a.length();)
-    {
-        if(a[i] == '[')
-        {
-            for(int j = i;j < a.length();)
-            {
-                if(a[j] == ']')
-                {
-                    p[countn] = a.substr(i + 1,j - i - 1);
-                    i = j + 1;
-                    countn++;
-                    break;
-                }
-                else j++;
-            }
-        }
-        else i++;
-    }
-    return p;
+    vector<string> v;
+    int i,j;
+    for(i = a.find('['),j = a.find(']');i != string::npos && j != string::npos;i = a.find('[',j+1),j = a.find(']',j+1)) v.push_back(a.substr(i+1,j-i-1));
+    return v;
+}
+
+bool judge(int &n,vector<int> v)
+{
+    if(n-1 >= 0 && n-1 < v.size()) return (!v[n-1].empty());
+    else return false;
 }
 
 int main()
 {
     string a,b,c;
-    getline(cin,a);getline(cin,b);getline(cin,c);
-    string (*ap) = createList(a);string (*bp) = createList(b);string (*cp) = createList(c);
+    getline(cin,a);
+    vector<string> av = createList(a);
+    getline(cin,b);
+    vector<string> bv = createList(b);
+    getline(cin,c);
+    vector<string> cv = createList(c);
     int n;
     cin>>n;
-    int n1,n2,n3,n4,n5;
     for(int i = 0;i < n;i++)
     {
+        int n1,n2,n3,n4,n5;
         cin>>n1>>n2>>n3>>n4>>n5;
-        if(!ap[n1].empty() && !ap[n5].empty())
+        if(!av[n1-1].empty() && !av[n5-1].empty())
         {
-            if(!bp[n2].empty() && !bp[n4].empty())
+            if(!bv[n2-1].empty() && !bv[n4-1].empty())
             {
-                if(!cp[n3].empty())
-                    cout<<ap[n1]<<"("<<bp[n2]<<cp[n3]<<bp[n4]<<")"<<ap[n5]<<endl;
+                if(!cv[n3-1].empty()) cout<<av[n1-1]<<"("<<bv[n2-1]<<cv[n3-1]<<bv[n4-1]<<")"<<av[n5-1]<<endl;
                 else cout<<"Are you kidding me? @\\/@"<<endl;
             }
             else cout<<"Are you kidding me? @\\/@"<<endl;
         }
         else cout<<"Are you kidding me? @\\/@"<<endl;
     }
-    delete [] ap;delete [] bp;delete [] cp;
 }
