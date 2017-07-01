8b334ae37ebd53c720becfed5123a12012c6d119
Jiajian Chen <chenjiajia9411@vip.qq.com>
2017-07-01 12:00:13 +0800

    解决一题。

--- B/1052.cpp
+++ B/1052.cpp
@@ -11,32 +11,29 @@ vector<string> createList(string& a)
     return v;
 }
 
-bool judge(int &n,vector<int> v)
+bool judge(int n,vector<string> &v)
 {
-    if(n-1 >= 0 && n-1 < v.size()) return (!v[n-1].empty());
+    if(n >= 0 && n < v.size()) return !v[n].empty();
     else return false;
 }
 
 int main()
 {
     string a,b,c;
-    getline(cin,a);
-    vector<string> av = createList(a);
-    getline(cin,b);
-    vector<string> bv = createList(b);
-    getline(cin,c);
-    vector<string> cv = createList(c);
+    getline(cin,a);vector<string> av = createList(a);
+    getline(cin,b);vector<string> bv = createList(b);
+    getline(cin,c);vector<string> cv = createList(c);
     int n;
     cin>>n;
     for(int i = 0;i < n;i++)
     {
         int n1,n2,n3,n4,n5;
         cin>>n1>>n2>>n3>>n4>>n5;
-        if(!av[n1-1].empty() && !av[n5-1].empty())
+        if(judge(n1-1,av) && judge(n5-1,av))
         {
-            if(!bv[n2-1].empty() && !bv[n4-1].empty())
+            if(judge(n2-1,bv) && judge(n4-1,bv))
             {
-                if(!cv[n3-1].empty()) cout<<av[n1-1]<<"("<<bv[n2-1]<<cv[n3-1]<<bv[n4-1]<<")"<<av[n5-1]<<endl;
+                if(judge(n3-1,cv)) cout<<av[n1-1]<<"("<<bv[n2-1]<<cv[n3-1]<<bv[n4-1]<<")"<<av[n5-1]<<endl;
                 else cout<<"Are you kidding me? @\\/@"<<endl;
             }
             else cout<<"Are you kidding me? @\\/@"<<endl;
